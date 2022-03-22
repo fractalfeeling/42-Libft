1b32bbabe09461254bedf28a145153137a987d81
shyen <shyen@ouroboros.modem>
2022-03-22 17:55:08 +1030

    update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lwee <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 15:03:41 by lwee              #+#    #+#             */
-/*   Updated: 2022/03/16 20:01:31 by lwee             ###   ########.fr       */
+/*   Updated: 2022/03/22 17:39:57 by lwee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,7 @@ int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
