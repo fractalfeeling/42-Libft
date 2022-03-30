42882144f2aa72e338a8d6c952278a1c2929598b
shyen <shyen@ouroboros.modem>
2022-03-30 20:39:51 +1030

    update

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: lwee <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 17:58:58 by lwee              #+#    #+#             */
-/*   Updated: 2022/03/30 18:14:17 by lwee             ###   ########.fr       */
+/*   Updated: 2022/03/30 20:32:07 by lwee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,20 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*new;
+	t_list	*begin;
+
+	begin = NULL;
+	while (lst)
+	{
+		new = ft_lstnew((f)(lst->content));
+		if (!new)
+		{
+			ft_lstclear(&begin, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&begin, new);
+		lst = lst->next;
+	}
+	return (begin);
+}
